91fdee2805ebfa20ee1ac3b2c7f082f0c543bfac
SpecialFriendRice <corinneborgeaud@hotmail.com>
2024-02-24 22:46:04 +0000

    end of day code tweaks

--- index.js
+++ index.js
@@ -6,23 +6,27 @@ const path = require("path");
 const fs = require("fs");
 
 
-//Path module methods (?) are used to where the resulting html file should go: into a directory called output. The two lines below were supplied as starter code and I have put them in the renderHTML function
+//These two path module methods are used to where dictate where the resulting html file should go: into a directory called output. The two lines below were supplied as starter code
+// Do they need to be in the renderHTML function?
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-//How is this render constant worked into renderHTML function?
 const render = require("./src/page-template.js");
 
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file. NOTE the HTML is already laid out in page-template.js
+// TODO: Write Code to gather information about the development team members, and render the HTML file. 
+//NOTE the HTML itself is already laid out in page-template.js
 
 //Cut and past from the README = Write code in index.js that uses inquirer to gather information about the development team members and creates objects for each team member using the correct classes as blueprints.
+//creating objects from classes will mean const emp1 = new Engineer (all parameters, comma separated) etc
+//Do I have to declare an empty array for team (let team = []) and push in each object (team member) as they are created? So team.push(emp1)
+//should the above declaring (?) a new object and then pushing to an array be a .then after each section of questions?
 
 
 //Rough prompt layout using Inquirer, wrapped in a function (need async keyword for sure?)
 //Do I need to use await?
 
-//is this now going to end up just a createMgr function? Shall I rename from promptUser?
+//is the first function below that I have as promptUser currently going to end up just a createMgr function? 
 
 async function promptUser() {
     return inquirer
@@ -53,9 +57,9 @@ async function promptUser() {
             //for Manager subclass only
         },
 
-        //WILL THE BELOW WORK? AT POINT OF CHOICES, SPECIFY NEW FUNCTION WITHIN A FUNCTION? I jhave now changed the function from promptUser to offerChoice - will this still work??? Where do I then call this new function?
+        //WILL THE BELOW WORK? AT POINT OF CHOICES, SPECIFY NEW FUNCTION WITHIN A FUNCTION? I have now changed the function from promptUser to offerChoice - will this still work??? Where do I then call this new function? Do I need ASYNC/AWAIT?
         async function offerChoice(){
-            const nextStep = await inquirer
+            const nextStep = await inquirer //really not sure of function and constant names yet!
             .prompt([
                 {
                     type: 'list',
@@ -64,17 +68,44 @@ async function promptUser() {
                     choices: ['Add Engineer', 'Add Intern', 'Finish and create org chart'],
                 },
 
+            ])
 
-            ])}
+            if (this.choices === 'Add Engineer') { //do I have to refer to choices array and index? Will using 'this' work?)
 
-            if (condition) {
+                //Cut and paste from the README:
+                // When a user selects the engineer option then a user is prompted to enter the following and then the user is taken back to the menu:
+                // Engineer's Name
+                // ID
+                // Email
+                // GitHub username
                 
+            } else if (this.choices === 'Add Intern') { //do I have to refer to choices array and index? Will using 'this' work?)
+                
+                //Cut and paste from the README:
+                // When a user selects the intern option then a user is prompted to enter the following and then the user is taken back to the menu:
+                // Intern’s name
+                // ID
+                // Email
+                // School
+
             } else {
-                
+                //just return? watch out for when render html
+
+                 //Cut and paste from the README:
+                // When a user decides to finish building their team then they exit the application, and the HTML is generated.
             }
+        },
 
 
-        //OR CAN I USE A VERY SIMPLE .then after the .prompt?? like below
+           
+
+          
+
+
+
+
+
+        //OR CAN I USE A SIMPLE .then after the .prompt?? Roughly as below
         // .then((responses) =>{
         //     if (responses.choices === this.choices[0]) { //can this refer to the index of the choices array? can it be paired with this?
         //         CODE FOR ADD ENGINEER
@@ -85,39 +116,17 @@ async function promptUser() {
         //     }
         // });      
 
-        // Cut and paste from README = When a user enters those requirements then the user is presented with a menu with the option to:
-        // Add an engineer
-        // Add an intern
-        // Finish building the team
-        //DOES THIS SIT INSIDE THE SAME FUNCTION OR ANOTHER? DO I NEED ASYNC/AWAIT?
     
        
-
-
-        
-
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Please enter your GitHub username',
-            //for Engineer subclass only
-        },
-
-       
-
-        {
-            type: 'input',
-            name: 'school',
-            message: 'Please enter your School name',
-            //for Intern subclass only
-        },
       ]);
     }
 
-    // Is ID autogenerated for each person? Does it have to correspond to the screengrab i.e. Managers are assigned first numbers and interns later numbers with Engineers in the middle?
+    // Cut and paste from README: Call the render function (provided for you) and pass in an array containing all employee objects;
+    // The render function will generate and return a block of HTML including templated divs for each employee
+    //render declaration from above:
+    //const render = require("./src/page-template.js");
 
-
-    //Code to render the html - where define data/input?
+    //Code to render the html:where define data/input?
     async function renderHTML(data) {
 
        // file path and directory path were already defined in supplied code using path node module
@@ -135,13 +144,9 @@ async function promptUser() {
         console.error(err);
       }
     
-      //Add user input (currently 'data' - does it need to be stringified?) to the html using fs's writeFile method
+      //Add user input (currently 'data' - does it need to be stringified?) to the html using fs's writeFile method?
      
-
       //code with callback catching errors is from node.js documentation. Again, how does this error callback effect testing?
-
-      //how do I pull in the render constant/html template supplied in starter code (as below)?
-      //const render = require("./src/page-template.js");
 
      //NOTE although it’s not a requirement, consider adding validation to ensure that user input is in the proper format.
 
@@ -153,9 +158,9 @@ async function promptUser() {
         }
       });
        
-    }
+    };
 
-    //function to run both promptUser and renderHTML
+    //function to run both promptUser and renderHTML - THIS NEED WILL HAVE CHANGED NOW
     //again is try and catch unnecessary with testing methods?
     // why the need to define userData here?
 
@@ -165,7 +170,7 @@ async function promptUser() {
         } catch (err) {
             console.error("This is your error from runBoth:", err)
         }
-    }
+    };
 
     runBoth();
 
