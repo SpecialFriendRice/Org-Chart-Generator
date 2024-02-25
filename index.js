a2ba1f3fd13d10272535d2dce29a9d682ac98b97
SpecialFriendRice <corinneborgeaud@hotmail.com>
2024-02-25 21:22:49 +0000

    prompt flow improved

--- index.js
+++ index.js
@@ -16,13 +16,14 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 //Function to create output directory if one does not exist already, and write the html string (= 'render') to a file called team.html within it
 //WHERE SHALL I CALL THE FUNCTION?
+
 function createHtmlFile(){
     if (!fs.existsSync(OUTPUT_DIR)) {
         fs.mkdirSync(OUTPUT_DIR);
       } else {
         fs.writeFileSync(outputPath, render) //definitely use outputPath, not 'team.html'? Is render already a string?? Would it need to go in backticks??
       }  
-}
+};
 
 //NOTE in node.js documentation, if statements are wrapped in a try/catch as per below example. Should I do this? 
 // try {
@@ -33,7 +34,7 @@ function createHtmlFile(){
 //     console.error(err);
 //   }
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file. Or as described in README "Write code in index.js that uses inquirer to gather information about the development team members and creates objects for each team member using the correct classes as blueprints".
+// TODO: Write Code to gather information about the development team members, and render the HTML file. Or as described in README "Write code in index.js that uses inquirer to gather information about the development team members and creates objects for each team member using the correct classes as blueprints..."When a user decides to finish building their team then they exit the application, and the HTML is generated.".
 
 //Creating objects from classes for each team member:
 //Do I have to declare an empty array for team and push in each object (team member) as they are created?
@@ -52,42 +53,41 @@ async function createMgr() {
       .prompt([
         {
             type: 'input',
-            name: 'mgrName',  //specifically avoid using just "name" for all classes?
+            name: 'name',  //not distinguish as mgrName?
             message: 'Enter name of Team Manager',
         },
     
         {
             type: 'input',
-            name: 'mgrId', //specifically avoid using just "id" for all classes?
+            name: 'id', //not distinguish as mgrId?
             message: 'Enter Employee ID of Manager',
         },
 
         {
             type: 'input',
-            name: 'mgrEmail', //specifically avoid using just "email" for all classes?
+            name: 'email', //not distinguish as mgrEmail?
             message: 'Enter email address of Manager',
             //haven't specified format e.g. @fakemail.com
         },
 
         {
             type: 'input',
-            name: 'offNum',
+            name: 'officeNumber',
             message: 'Enter office number of Manager',
             //for Manager subclass only
         },
     ])
-
-        .then //Then create a new object and push to the team array. what else here?? Can a then sit inside this function now I've put a nested function in it?
-        const newManager = new Manager (Manager.id, Manager.name, Manager.email, Manager.officeNumber);
+        //why is the below not working (it was originally in a .then)?
+        const newManager = new Manager (Manager.name, Manager.id, Manager.email, Manager.officeNumber);
         team.push(newManager);
         offerChoice();
     };
-    createMgr();
+
 
     //offerChoice() below was originally a function within a function.
     // Do I need ASYNC/AWAIT? e.g. await inquirer? Should I use a switch statement inside a while loop?
 
-        function offerChoice(){
+        function offerChoice() {
             const nextStep = inquirer //not sure of function and constant names yet!
             .prompt([
                 {
@@ -97,25 +97,25 @@ async function createMgr() {
                     choices: ['Add Engineer', 'Add Intern', 'Finish and create org chart'],
                 },
 
-            ])
+            ]).then(nextStep => { 
 
             if (nextStep.choiceStep === 'Add Engineer') { //was wondering if I needed to refer to choices[index]
                 inquirer.prompt([
                     {
                         type: 'input',
-                        name: 'engName',  //specifically avoid using just "name" for all classes?
+                        name: 'name',  //not distinguish as engName?
                         message: 'Enter name of Engineer',    
                     },
 
                     {
                         type: 'input',
-                        name: 'engId',  //specifically avoid using just "id" for all classes?
+                        name: 'id',  //not distinguish as engId?
                         message: 'Enter Employee ID of Engineer',    
                     },
                     
                     {
                         type: 'input',
-                        name: 'engEmail',  //specifically avoid using just "email" for all classes?
+                        name: 'email',  //not distinguish as engEmail?
                         message: 'Enter email address of Engineer',    
                     },
 
@@ -123,45 +123,32 @@ async function createMgr() {
                         type: 'input',
                         name: 'github',  //capitalisation?
                         message: 'Enter GitHub username of Engineer',    
-                    },
-               // .then 
-                //take back to the choiceStep - do I need to do this by return/break or should I have broken the function(s) at a different place?
+                    }
 
-                ]);
-                //Cut and paste from the README:
-                // When a user selects the engineer option then a user is prompted to enter the following and then the user is taken back to the menu:
-                // Engineer's Name
-                // ID
-                // Email
-                // GitHub username
+                ]).then (Engineer => { //is Engineer the correct argument here? 
+                const newEngineer = new Engineer (Engineer.name, Engineer.id, Engineer.email, Engineer.github);
+                team.push(newEngineer);
+                offerChoice();
 
-
+               });
                 
             } else if (nextStep.choiceStep === 'Add Intern') { //was wondering if I needed to refer to choices[index]
-                
-                //Cut and paste from the README:
-                // When a user selects the intern option then a user is prompted to enter the following and then the user is taken back to the menu:
-                // Intern’s name
-                // ID
-                // Email
-                // School
-
                 inquirer.prompt([
                     {
                         type: 'input',
-                        name: 'intName',  //specifically avoid using just "name" for all classes?
+                        name: 'name',  //not distinguish as intName?
                         message: 'Enter name of Intern',    
                     },
 
                     {
                         type: 'input',
-                        name: 'intId',  //specifically avoid using just "id" for all classes?
+                        name: 'id',  //not distinguish as intId?
                         message: 'Enter Employee ID of Intern',    
                     },
                     
                     {
                         type: 'input',
-                        name: 'intEmail',  //specifically avoid using just "email" for all classes?
+                        name: 'email',  //not distinguish as intEmail?
                         message: 'Enter email address of Intern',    
                     },
 
@@ -170,63 +157,49 @@ async function createMgr() {
                         name: 'school', 
                         message: 'Enter name of school or university loaning Intern',    
                     },
-                
-                //.then 
-                //take back to the choiceStep - do I need to do this by returning somehow or should I have broken the function(s) at a different place?
-            ]);
 
-            } else {
-                //RENDER render(team)? 
+                ]).then (Intern => { //is Intern the correct argument here?
+                    const newIntern = new Intern (Intern.name, Intern.id, Intern.email, Intern.school);
+                    team.push(newIntern);
+                    offerChoice();
+                 });
+
+            } else { 
                 //return/break/exit? 
                 console.log("Your org chart will now be generated");
-                createHtmlFile();
-                 //Cut and paste from the README "When a user decides to finish building their team then they exit the application, and the HTML is generated.""
+                createHtmlFile(render(team));
+                 
             }
-        };
-
-
-        //OR CAN I USE A SIMPLE .then after the .prompt?? Roughly as below
-        // .then((responses) =>{
-        //     if (nextStep.choiceStep === ??????) { 
-        //         CODE FOR ADD ENGINEER
-        //     } else if(responses.choices === this.choices[1]) {
-        //         CODE FOR ADD INTERN 
-        //     } else {
-        //         CODE FOR FINISH AND CREATE AN ORG CHART
-        //     }
-        // });      
-
-    
-       
-    
+            
+        });
+        }
+        createMgr();
     
 
+     
+
+
+    //is the below now all redundant?
     //Code to render the html: 
-    //in place of 'data' do I need 'team' or 'render' (in which format?) or both/neither? Do I render(team)?    
-    function createHtmlFile(data) { 
+    //in place of 'data' do I need 'team' or 'render' (in which format?) or both/neither? Do I render(team)? 
+    // function createHtmlFile(data) { 
 
-        const OUTPUT_DIR = path.resolve(__dirname, "output");
-        const outputPath = path.join(OUTPUT_DIR, "team.html");
+    //     const OUTPUT_DIR = path.resolve(__dirname, "output");
+    //     const outputPath = path.join(OUTPUT_DIR, "team.html");
        
-       try {
-        if (!fs.existsSync(OUTPUT_DIR)) {
-          fs.mkdirSync(OUTPUT_DIR);
-        }
-      } catch (err) {
-        console.error(err);
-      }
+    //    try {
+    //     if (!fs.existsSync(OUTPUT_DIR)) {
+    //       fs.mkdirSync(OUTPUT_DIR);
+    //     }
+    //   } catch (err) {
+    //     console.error(err);
+    //   }
 
-      fs.writeFile(outputPath, data, err =>{
-        if (err) {
-            console.error("This is your error writing to file:", err);
-        } else {
-            console.log("Your data has been written to:", outputPath);
-        }
-      });
-       
-    };
-
-
-//module.exports?
-  
-
+    //   fs.writeFile(outputPath, data, err =>{
+    //     if (err) {
+    //         console.error("This is your error writing to file:", err);
+    //     } else {
+    //         console.log("Your data has been written to:", outputPath);
+    //     }
+    //   }); 
+    // }
