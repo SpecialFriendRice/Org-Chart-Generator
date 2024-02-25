40d13eb347b2028cd4f5821de32b672ea095a437
SpecialFriendRice <corinneborgeaud@hotmail.com>
2024-02-25 17:08:10 +0000

    more prompt code

--- index.js
+++ index.js
@@ -5,32 +5,48 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+//HTML itself is already laid out in page-template.js, which is grabbed below (why is this variable given the name render? is it in the noun sense i.e. the rendered team) The module.exports from page-template is a function that returns an object that is a string of html; as the function is anonymous, it can be called anything when it's imported into another file, hence 'render' here)
+const render = require("./src/page-template.js");
 
-//These two path module methods are used to where dictate where the resulting html file should go: into a directory called output. The two lines below were supplied as starter code
+//STARTER CODE The two path module methods 'resolve' and 'join' are used to where dictate where the resulting html file should go: into a directory called output. 
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-// Do the above need to be in the (current) renderHTML function? Where is the function creating the output directory?
+//Function to create output directory if one does not exist already, and write the html string (= 'render') to a file called team.html within it
+//WHERE SHALL I CALL THE FUNCTION?
+function createHtmlFile(){
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+      } else {
+        fs.writeFileSync(outputPath, render) //definitely use outputPath, not 'team.html'?
+      }  
+}
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file. 
-//NOTE the HTML itself is already laid out in page-template.js, which is grabbed below
-const render = require("./src/page-template.js");
+//NOTE in node.js documentation, if statements are wrapped in a try/catch as per below example. Should I do this? 
+// try {
+//     if (!fs.existsSync(OUTPUT_DIR)) {
+//       fs.mkdirSync(OUTPUT_DIR);
+//     }
+//   } catch (err) {
+//     console.error(err);
+//   }
 
+ 
 
-//Cut and past from the README = Write code in index.js that uses inquirer to gather information about the development team members and creates objects for each team member using the correct classes as blueprints.
-//creating objects from classes will mean const emp1 = new Engineer (all parameters, comma separated) etc
-//Do I have to declare an empty array for team (let team = []) and push in each object (team member) as they are created? So team.push(emp1). Is the array called team - check tests
+// TODO: Write Code to gather information about the development team members, and render the HTML file. Cut and past from README = Write code in index.js that uses inquirer to gather information about the development team members and creates objects for each team member using the correct classes as blueprints.
+
+//Creating objects from classes for each team member:
+//Do I have to declare an empty array for team and push in each object (team member) as they are created?
 let team = [];
-//should the above declaring (?) a new object and then pushing to an array be a .then after each section of questions?
-//the below is using the syntax from the page-template - will this work with template literals? Probably not as there will be numbers like for employee id, but can they be treated as strings? Where can I 
-const newManager = new Manager (${manager.getId()}, ${manager.getName()}, ${manager.getEmail()}, ${manager.getOfficeNumber()})
+//should the below declaring (?) a new instance of a class (Manager, Engineer or Intern), then pushing to an array be in the format of a .then after each section of questions? OR in a separate function afterwards?
+//is the below the correct syntax for properties?
+const newManager = new Manager (Manager.id, Manager.name, Manager.email, Manager.officeNumber);
 team.push(newManager);
 
 
 //Rough prompt layout using Inquirer, wrapped in a function (need async keyword for sure?)
 //Do I need to use await?
-
 //is the first function below that I have as promptUser currently going to end up just a createMgr function? 
 
 async function promptUser() {
@@ -58,7 +74,7 @@ async function promptUser() {
         {
             type: 'input',
             name: 'offNum',
-            message: 'Please enter your office number',
+            message: 'Enter office number of Manager',
             //for Manager subclass only
         },
 
@@ -75,16 +91,45 @@ async function promptUser() {
 
             ])
 
-            if (this.choices === 'Add Engineer') { //do I have to refer to choices array and index? Will using 'this' work?)
+            if (nextStep.choiceStep === 'Add Engineer') { //was wondering if I needed to refer to choices[index]
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'engName',  //specifically avoid using just "name" for all classes?
+                        message: 'Enter name of Engineer',    
+                    },
 
+                    {
+                        type: 'input',
+                        name: 'engId',  //specifically avoid using just "id" for all classes?
+                        message: 'Enter Employee ID of Engineer',    
+                    },
+                    
+                    {
+                        type: 'input',
+                        name: 'engEmail',  //specifically avoid using just "email" for all classes?
+                        message: 'Enter email address of Engineer',    
+                    },
+
+                    {
+                        type: 'input',
+                        name: 'github',  //capitalisation?
+                        message: 'Enter GitHub username of Engineer',    
+                    },
+                //.then 
+                //take back to the choiceStep - do I need to do this by returning somehow or should I have broken the function(s) at a different place?
+
+                ]);
                 //Cut and paste from the README:
                 // When a user selects the engineer option then a user is prompted to enter the following and then the user is taken back to the menu:
                 // Engineer's Name
                 // ID
                 // Email
                 // GitHub username
+
+
                 
-            } else if (this.choices === 'Add Intern') { //do I have to refer to choices array and index? Will using 'this' work?)
+            } else if (nextStep.choiceStep === 'Add Intern') { //was wondering if I needed to refer to choices[index]
                 
                 //Cut and paste from the README:
                 // When a user selects the intern option then a user is prompted to enter the following and then the user is taken back to the menu:
@@ -93,6 +138,34 @@ async function promptUser() {
                 // Email
                 // School
 
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'intName',  //specifically avoid using just "name" for all classes?
+                        message: 'Enter name of Intern',    
+                    },
+
+                    {
+                        type: 'input',
+                        name: 'intId',  //specifically avoid using just "id" for all classes?
+                        message: 'Enter Employee ID of Intern',    
+                    },
+                    
+                    {
+                        type: 'input',
+                        name: 'intEmail',  //specifically avoid using just "email" for all classes?
+                        message: 'Enter email address of Intern',    
+                    },
+
+                    {
+                        type: 'input',
+                        name: 'school',  //capitalisation?
+                        message: 'Enter name of school or university loaning Intern',    
+                    },
+                //.then 
+                //take back to the choiceStep - do I need to do this by returning somehow or should I have broken the function(s) at a different place?
+            ]);
+
             } else {
                 //just return? watch out for when render html
 
@@ -100,14 +173,6 @@ async function promptUser() {
                 // When a user decides to finish building their team then they exit the application, and the HTML is generated.
             }
         },
-
-
-           
-
-          
-
-
-
 
 
         //OR CAN I USE A SIMPLE .then after the .prompt?? Roughly as below
@@ -139,7 +204,7 @@ async function promptUser() {
         const OUTPUT_DIR = path.resolve(__dirname, "output");
         const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-       //Need to mkdir "output" using fs. The try and catch methods wrapper below comes from node.js documentation. Can it be combined with the existing constants like this? Is this redundant if running tests?
+       //Need to mkdir "output" using fs. The try and catch methods wrapper below comes from node.js documentation, but can I do without it? Is this redundant if running tests, or are tests just on classes?
        
        try {
         if (!fs.existsSync(OUTPUT_DIR)) {
@@ -165,6 +230,13 @@ async function promptUser() {
        
     };
 
+    // is 'data' in the above going to need to be defined as a string in backticks and is it the contents of page-template.js - the latter is in the module.exports of page-template but how do I call (?) it?
+    //is the below an alternative to the above?
+  
+    fs.writeFileSync(outputPath, `HTML string goes in here, in backticks`) 
+
+
+
     //function to run both promptUser and renderHTML - THIS NEED WILL HAVE CHANGED NOW
     //again is try and catch unnecessary with testing methods?
     // why the need to define userData here?
@@ -179,5 +251,23 @@ async function promptUser() {
 
     runBoth();
 
+
+    //_____________________________________________//
+
+//shall I just make one function to create the team.html file?
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+function createHtmlFile(){
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+      } else {
+        fs.writeFileSync(outputPath, render) 
+      }  
+}
+//where does the above function run?
+
+//module.exports?
   
 
